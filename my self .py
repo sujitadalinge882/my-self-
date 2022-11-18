f90fd4a086cb42d92ce8f5977129e6034d0cc5e9
sujit vijay adalinge <110627737+sujitadalinge882@users.noreply.github.com>
2022-11-18 22:40:01 +0530

    Add files via upload

---
+++ my self .py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+Good morning Sir.First of all thank you for giving me this opportunity to introduce myself. My name is sujit.
+I have completed my diploma from K.B.P.Polytrchnic Satara specialization in 
+electrical engineering .I am a person with critical thinking skills and have the ability to turn a regular
+project into something innovative.My final year major project is based on speed control of an induction motor 
+using composer studio. I have good circuitry knowledge.I have recently done python project that is "Food delivery app"
+I have 1 year experience in "TATA MOTORS PVT LTD ".currently I am working in "TORK MOTORS PVT LTD" as a team leader.
+I have always been a quick learner and i like photography,trekking,watching tv , gaming.
+I have participated in many technical event in my college and I have excellent leadership skills.
+I have learned how to work as a team.I have always been interested in computer language like python,SQL ,data science.
+I am excited at that thought of the opportunity to apply my technical skills and experience in your company and 
+improve my skill further.I also posses good communication skills.I look forward to working in a reputed company. 
+thats all about myself.thank you.
+
